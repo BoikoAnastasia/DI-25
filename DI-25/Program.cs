01fffb5736f7da3e332c38da828e485a58725e52
RomanObraztsov <obraztsov.rs.di25@mail.ru>
2024-06-05 14:05:01 +0300

    obraztsov_lesson1_1_1

--- DI-25/Program.cs
+++ DI-25/Program.cs
@@ -1,5 +1,3 @@
-// test
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -24,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=start}/{action=hello}/{id?}");
 
 app.Run();
