28515de7bf0c0a81e13336586a7e6dcb4f1fddf5
ZernovMA <Zernovmix@gmail.com>
2024-06-05 13:59:12 +0300

    ..

--- DI-25/Controllers/startController.cs
+++ DI-25/Controllers/startController.cs
@@ -31,14 +31,7 @@ namespace Di_25.Controllers
         }
         public string hello(int a, int b)
         {
-            if a = null 
-                { 
-                    a = 0; 
-                }
-            if b = null 
-                { 
-                    b = 0; 
-                }
+          
             return $"Ответ {a} + {b} = {a + b}";
         }
     }
