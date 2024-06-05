d8ec63cfe123ecbc3ce4404e498526a47c85b654
RomanObraztsov <obraztsov.rs.di25@mail.ru>
2024-06-05 14:23:34 +0300

    obraztsov_lesson1_1_3

--- DI-25/Controllers/CalculatorController.cs
+++ DI-25/Controllers/CalculatorController.cs
@@ -3,10 +3,30 @@ using System.Diagnostics;
 
 namespace Di_25.Controllers;
 
-[ApiController]
 [Route("calculator")]
 public class CalculatorController : ControllerBase
 {
-    [HttpGet("index/{arg1:int?}/{arg2:int?}")]
-    public string Get(int arg1 = 0, int arg2 = 0) => $"{arg1} + {arg2} = {arg1 + arg2}";
+    [HttpGet("index/{arg1:int?}/{arg2:int?}/{operation?}")]
+    public string Get(int arg1 = 0, int arg2 = 0, string operation = "+")
+    {
+        int result;
+
+        switch (operation)
+        {
+            case "+":
+                result = arg1 + arg2;
+                break;
+            case "-":
+                result = arg1 - arg2;
+                break;
+            case "*":
+                result = arg1 * arg2;
+                break;
+            default:
+                result = arg1 + arg2;
+                break;
+        }
+
+        return $"{arg1} {operation} {arg2} = {result}";
+    }
 }
