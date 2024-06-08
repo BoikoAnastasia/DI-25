c5ef24a9ddcf4bca7264cc283cfc3029412df5a1
Val200ya <australia636@gmail.com>
2024-06-08 17:39:43 +0300

    Задание 4 выполнено

---
+++ DI-25/Controllers/CalcController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Di_25.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            if (c == "+")
+            {
+                return a + " + " + b + " = " + (a + b);
+            }
+            else if (c == "-")
+            {
+                return a + " - " + b + " = " + (a - b);
+            }
+            else if (c == "*")
+            {
+                return a + " * " + b + " = " + (a * b);
+            }
+            else if (c == "/")
+            {
+                return a + " / " + b + " = " + (a / b);
+            }
+            else if (c == null)
+            {
+                return a + " + " + b + " = " + (a + b);
+            }
+            else
+            {
+                return "Пожайлуста, выберите +, -, * или /";
+            }
+        }
+    }
+}
