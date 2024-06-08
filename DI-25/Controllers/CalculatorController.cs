b7b91d560c13aad7052ab609c3c46e1ce6721ac9
Val200ya <australia636@gmail.com>
2024-06-08 17:16:45 +0300

    Задание 3 выполнено

---
+++ DI-25/Controllers/CalculatorController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Di_25.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            if (c == "+")
+            {
+                return a + " + " + b + " = " + (a + b);
+            }
+            else if (c == "-")
+            {
+                return a + " - " + b + " = " + (a - b);
+            }
+            else if (c == "*")
+            {
+                return a + " * " + b + " = " + (a * b);
+            }
+            else if (c == null)
+            {
+                return a + " + " + b + " = " + (a + b);
+            }
+            else
+            {
+                return "Пожайлуста, выберите +, - или *";
+            }
+        }
+    }
+}
