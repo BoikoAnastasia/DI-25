678911aa6927d201d1b593e51bca79731e00f4f7
ZernovMA <zernovmix@gmail.com>
2024-06-08 16:13:00 +0300

    .

--- DI-25/Program.cs
+++ DI-25/Program.cs
@@ -22,7 +22,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=calculator}/{action=Index}/{a?}/{b?}/{c?}");
 
 app.Run();
 //a
