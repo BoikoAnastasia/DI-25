e46fa858eab63c19bb5c5bb770402ae1ccb6bd02
Val200ya <australia636@gmail.com>
2024-06-08 16:33:06 +0300

    Задание 2 выполнено

---
+++ DI-25/Controllers/CalculatorController.cs
@@ -0,0 +1,15 @@
+﻿using Di_25.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Di_25.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b)
+        {
+            return a + " + " + b + " = " + (a + b);
+        }
+    }
+}
