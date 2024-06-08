043a050fcdd46f349c46c3459ae0ac5bceb6f9fa
Val200ya <australia636@gmail.com>
2024-06-08 15:42:45 +0300

    Задача выполнена

---
+++ DI-25/Controllers/StartController.cs
@@ -0,0 +1,55 @@
+﻿using Di_25.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Di_25.Controllers
+{
+    public class StartController : Controller
+    {
+        private readonly ILogger<StartController> _logger;
+
+        public StartController(ILogger<StartController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public string Hello()
+        {
+            int hour = DateTime.Now.Hour;
+
+            if (hour >= 0 && hour < 6)
+            {
+                return "Доброй ночи";
+            }
+            else if (hour >= 6 && hour < 12)
+            {
+                return "Доброе утро";
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                return "Добрый день";
+            }
+            else
+            {
+                return "Добрый вечер";
+            }
+        }
+    }
+}
