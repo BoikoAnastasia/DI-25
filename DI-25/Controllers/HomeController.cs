3a2b143f6cd9957317860a97f8a30e71e591e9a5
Val200ya <australia636@gmail.com>
2024-06-10 17:46:28 +0300

    Задание 6 выполнено

--- DI-25/Controllers/HomeController.cs
+++ DI-25/Controllers/HomeController.cs
@@ -6,14 +6,6 @@ namespace Di_25.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index() { 
-            string product1 = ProductsList.Products[0].Id.ToString() + "\n" + ProductsList.Products[0].Name + "\n" +
-                ProductsList.Products[0].Cost + "\n\n";
-            string product2 = ProductsList.Products[1].Id.ToString() + "\n" + ProductsList.Products[1].Name + "\n" +
-                ProductsList.Products[1].Cost + "\n\n";
-            string product3 = ProductsList.Products[2].Id.ToString() + "\n" + ProductsList.Products[2].Name + "\n" +
-                ProductsList.Products[2].Cost + "\n\n";
-            return product1 + product2 + product3;
-        }
+        
     }
 }
--- DI-25/Controllers/Product.cs
+++ DI-25/Controllers/Product.cs
@@ -5,12 +5,13 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Cost { get; set; }
-        //public string Description { get; set; }
-        public Product(int id, string name, decimal cost)
+        public string Description { get; set; }
+        public Product(int id, string name, decimal cost, string description)
         {
             Id = id;
             Name = name;
             Cost = cost;
+            Description = description;
         }
     }
 }
