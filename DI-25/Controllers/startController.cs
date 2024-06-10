6d94a174aaa1c5846dbdee407de34b900f04bbfb
admin <admin@3218EC11>
2024-06-10 13:59:26 +0300

    123

--- DI-25/Controllers/startController.cs
+++ DI-25/Controllers/startController.cs
@@ -7,49 +7,31 @@ namespace Di_25.Controllers
 {
     public class startController : Controller
     {
-        private readonly ILogger<startController> _logger;
-
-        public startController(ILogger<startController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-        public string hello()
-        {
-            DateTime dateTime = DateTime.Now;
-            int hour = dateTime.Hour;
-
-            if (hour >= 0 && hour <= 5)
+            public ActionResult GetGreeting()
             {
-                return "Доброй ночи";
-            }
-            else if (hour >= 6 && hour <= 11)
-            {
-                return "Доброе утро";
-            }
-            else if (hour >= 12 && hour <= 17)
-            {
-                return "Добрый день";
-            }
-            else
-            {
-                return "Добрый вечер";
+                DateTime currentTime = DateTime.Now;
+                string greeting = "";
+
+                if (currentTime.Hour >= 0 && currentTime.Hour < 6)
+                {
+                    greeting = "Доброй ночи";
+                }
+                else if (currentTime.Hour >= 6 && currentTime.Hour < 12)
+                {
+                    greeting = "Доброе утро";
+                }
+                else if (currentTime.Hour >= 12 && currentTime.Hour < 18)
+                {
+                    greeting = "Добрый день";
+                }
+                else
+                {
+                    greeting = "Добрый вечер";
+                }
+
+                return Content(greeting);
             }
         }
     }
-}
+
+
