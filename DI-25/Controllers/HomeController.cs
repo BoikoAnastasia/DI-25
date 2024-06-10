bf3d9ec2fe6b76dd6148484536ff234ee26a4061
RomanObraztsov <obraztsov.rs.di25@mail.ru>
2024-06-10 13:31:43 +0300

    obraztsov_lesson1_1_6

--- DI-25/Controllers/HomeController.cs
+++ DI-25/Controllers/HomeController.cs
@@ -13,10 +13,6 @@ namespace Di_25.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
 
         public IActionResult Privacy()
         {
@@ -27,6 +23,32 @@ namespace Di_25.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public String index(int a)
+        {
+            var answer = "";
+            for (int i = 0; i < ProductList.products.Count; i++)
+            {
+                answer = "";
+                if (ProductList.products[i].Id == a)
+                {
+                    answer += ProductList.products[i].Id;
+                    answer += "\n";
+                    answer += ProductList.products[i].Name;
+                    answer += "\n";
+                    answer += ProductList.products[i].Price;
+                    answer += "\n";
+                    answer += ProductList.products[i].Description;
+                    return answer;
+                    break;
+                }
+                else
+                {
+                    answer = "Продукта с таким id нет";
+                }
+            }
+            return answer;
         }
     }
 }
---
+++ DI-25/ProductList.cs
@@ -0,0 +1,12 @@
+﻿namespace Di_25
+{
+    public static class ProductList
+    {
+        public static List<Product> products = new List<Product>()
+        {
+            new Product(1, "Чай", 100, "С сахаром"),
+            new Product(2, "Кофе", 250, "Вкусный"),
+            new Product(3, "Лимонад", 500, "Дюшес")
+        };
+    }
+}
