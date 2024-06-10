0d51ebc11295640aa0ab1a0d29c9ffc6037d7d6c
admin <admin@3218EC11>
2024-06-10 14:08:24 +0300

    123

--- DI-25/Controllers/HomeController.cs
+++ DI-25/Controllers/HomeController.cs
@@ -1,31 +1,46 @@
 ﻿using Di_25.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
 
-public class Calculator : Controller
+namespace Di_25.Controllers
 {
-    public string Index(int a, int b, char operation)
+    public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
 
-        string result = " ";
-
-        switch (operation)
+        public HomeController(ILogger<HomeController> logger)
         {
-            case '+':
-                return $"{a} + {b} = {a + b}";
-                break;
-            case '-':
-                return $"{a} - {b} = {a - b}";
-                break;
-            case '*':
-                return $"{a} * {b} = {a * b}";
-                break;
-            case '\0':
-                return $"{a} + {b} = {a + b}";
-                break;
-            default:
-                return $"ура";
+            _logger = logger;
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public String index(int a)
+        {
+            var answer = "";
+            for (int i = 0; i < Products.kursy.Count; i++)
+            {
+                answer += Products.kursy[i].ID;
+                answer += "\n";
+                answer += Products.kursy[i].Name;
+                answer += "\n";
+                answer += Products.kursy[i].Cost;
+                answer += "\n";
+                answer += Products.kursy[i].Description;
+                answer += "\n";
+                answer += "\n";
+            }
+            return answer;
         }
     }
 }
