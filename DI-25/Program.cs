a3ee6dda9ead37afa48ee44e1e1247008fffca22
serenexva <evpatijk692@gmail.com>
2024-06-14 14:22:13 +0300

    z

--- DI-25/Program.cs
+++ DI-25/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=start}/{action=hello}/{id?}");
+    pattern: "{controller=Calc}/{action=Index}/{id?}");
 
 app.Run();
