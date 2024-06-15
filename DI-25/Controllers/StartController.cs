22d8bf50df3ab168c1f9c47d2b4810c7b4573e3e
Daniil Senokosov <nw.finn@yandex.ru>
2024-06-15 15:06:29 +0300

    123

---
+++ DI-25/Controllers/StartController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Di_25.Controllers
+{
+    public class StartController : Controller
+    {
+        public string hello()
+        {
+            DateTime dateTime = DateTime.Now;
+            int hour = dateTime.Hour;
+
+            if (hour >= 0 && hour <= 5)
+            {
+                return "Доброй ночи";
+            }
+            else if (hour >= 6 && hour <= 11)
+            {
+                return "Доброе утро";
+            }
+            else if (hour >= 12 && hour <= 17)
+            {
+                return "Добрый день";
+            }
+            else
+            {
+                return "Добрый вечер";
+            }
+        }
+    }
+}
