5e33666f735d30e451d1e9442b906f261f705382
Daniil Senokosov <nw.finn@yandex.ru>
2024-06-15 15:19:43 +0300

    gordienko_lesson_1_1_4

---
+++ DI-25/Controllers/Calc.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Di_25.Controllers
+{
+    public class Calc : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            if (c == "+")
+            {
+                return a + " + " + b + " = " + (a + b);
+            }
+            else if (c == "-")
+            {
+                return a + " - " + b + " = " + (a - b);
+            }
+            else if (c == "*")
+            {
+                return a + " * " + b + " = " + (a * b);
+            }
+            else if (c == "/")
+            {
+                return a + " / " + b + " = " + (a / b);
+            }
+            else if (c == null)
+            {
+
+                return a + " + " + b + " = " + (a + b);
+            }
+            else
+            {
+                return "Вам необходимо правильно задать операцию";
+            }
+        }
+    }
+}
