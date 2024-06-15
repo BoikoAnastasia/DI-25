544fef4fc333c437dad90e7dfba24ce01392c61f
Daniil Senokosov <nw.finn@yandex.ru>
2024-06-15 15:01:56 +0300

    AW

--- DI-25/Program.cs
+++ DI-25/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=start}/{action=hello}/{id?}");
+    pattern: "{controller=Home}/{action=index}/{id?}");
 
 app.Run();
