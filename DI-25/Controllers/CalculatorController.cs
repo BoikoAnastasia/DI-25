d37f982f24139faf3b22e322ee0756315a237dfc
Daniil Senokosov <nw.finn@yandex.ru>
2024-06-15 15:11:04 +0300

    gordienko_lesson_1_1_2

---
+++ DI-25/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Di_25.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(int a, int b)
+        {
+            return a + " + " + b + " = " + (a + b);
+        }
+    }
+}
