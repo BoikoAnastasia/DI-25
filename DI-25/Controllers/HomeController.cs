74640190aa223b3e315a2eb40a962fcc95498d9f
RomanObraztsov <144007530+RomanObraztsov@users.noreply.github.com>
2024-06-17 11:29:40 +0300

    Update HomeController.cs

--- DI-25/Controllers/HomeController.cs
+++ DI-25/Controllers/HomeController.cs
@@ -22,8 +22,7 @@ namespace Di_25.Controllers
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
