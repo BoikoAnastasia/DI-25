ea2511468582dc191cf2bf8180d88bb8e8dd58a9
RomanObraztsov <144007530+RomanObraztsov@users.noreply.github.com>
2024-06-17 11:32:37 +0300

    Update CalculatorController.cs

--- DI-25/Controllers/CalculatorController.cs
+++ DI-25/Controllers/CalculatorController.cs
@@ -3,10 +3,8 @@ using System.Diagnostics;
 
 namespace Di_25.Controllers;
 
-[ApiController]
-[Route("calculator")]
+
 public class CalculatorController : ControllerBase
 {
-    [HttpGet("index/{arg1:int?}/{arg2:int?}")]
     public string Get(int arg1 = 0, int arg2 = 0) => $"{arg1} + {arg2} = {arg1 + arg2}";
 }
