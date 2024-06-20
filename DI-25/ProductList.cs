aa4e3ef73e649d27631d85c298b9a6f8d4f789d7
Daniil Senokosov <nw.finn@yandex.ru>
2024-06-20 22:25:02 +0300

    gordienko_lesson_3_2

--- DI-25/ProductList.cs
+++ DI-25/ProductList.cs
@@ -4,10 +4,10 @@
     {
         public static List<Product> products = new List<Product>()
         {
-			new Product(1, "ромашки", 1000, "", 1),
-			new Product(2, "пионы", 2000, "", 1),
-			new Product(3, "розы", 3000, "", 1),
-			new Product(4, "фиалки", 1500, "", 1),
+			new Product(1, "ромашки", 1200, "", 0),
+			new Product(2, "пионы", 2500, "", 0),
+			new Product(3, "розы", 3100, "", 0),
+			new Product(4, "фиалки", 1500, "", 0),
 
 		};
     }
