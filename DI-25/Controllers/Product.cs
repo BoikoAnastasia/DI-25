4ceec98d700ee8552b4dfca9b1bafa7b267a84ce
xaxaxsxs <xomiachok.ziuzin2016@yandex.ru>
2024-06-21 13:44:12 +0300

    .

--- DI-25/Controllers/Product.cs
+++ DI-25/Controllers/Product.cs
@@ -7,15 +7,17 @@
         public decimal Price { get; set; }
         public string Description { get; set; }
         public int Value { get; set; }
+        piblic string ShortDescription { get; set; }
 
 
-        public Product(int id, string name, decimal price, string description, int value)
+        public Product(int id, string name, decimal price, string description, int value, string shortDescription)
         {
             Id = id;
             Name = name;
             Price = price;
             Description = description;
             Value = value;
+            ShortDescription = shortDescription;
         }
 
     }
