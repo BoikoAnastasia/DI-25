b37385c3bb9979f41db363ec76395e049b9384fd
ZernovMA <zernovmix@gmail.com>
2024-06-21 03:54:37 +0300

    .

--- DI-25/Controllers/HomeController.cs
+++ DI-25/Controllers/HomeController.cs
@@ -36,6 +36,15 @@ namespace Di_25.Controllers
 
         }
 
+        public IActionResult ProductDetails(int id)
+        {
+            var product = ProductList.products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
         public IActionResult Privacy()
         {
             return View();
